7b9d10ec98347750604005b5e4b9feb0f007eff5
Heeb <iheberj@gmail.com>
2018-11-11 18:44:07 -0600

    finished

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,44 +1,44 @@
 var friends = [
     {
-        name: 'Erisdar The Mysterious One',
+        name: 'Phenious',
         photo:
-            'https://forum.unity3d.com/proxy.php?image=http%3A%2F%2Fwww.infinitypbr.com%2Fdragon%2Fhd6.jpg&hash=572c8a1450b0670344e0da08cae18d18',
+            'https://upload.wikimedia.org/wikipedia/en/5/52/Phineas_Flynn.png',
         scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     },
     {
-        name: 'Mistical Taytay',
+        name: 'Ferb',
         photo:
-            'http://rizalprovince.ph/tourism_pictures/Avilon%20MZP%20-%20Montalban%20(2).jpg',
+            'https://upload.wikimedia.org/wikipedia/en/b/ba/Ferb_Fletcher.png',
         scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
     },
     {
-        name: 'Skylar Rose',
+        name: 'Candace',
         photo:
-            'http://i2.wp.com/northstarz.ca/wp-content/uploads/2013/12/Rose.jpg',
+            'https://upload.wikimedia.org/wikipedia/en/d/df/Candace_Flynn.PNG',
         scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
     },
     {
-        name: 'Colonel Paul',
+        name: 'Perry the Platypus',
         photo:
-            'http://www.153aw.ang.af.mil/shared/media/bio/Colonel%20Paul%20Lyman.jpg',
+            'https://upload.wikimedia.org/wikipedia/en/d/dc/Perry_the_Platypus.png',
         scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
     },
     {
-        name: 'Edward Snowden',
+        name: 'Dr. Heinz Doofenshmirtz',
         photo:
-            'https://upload.wikimedia.org/wikipedia/commons/6/60/Edward_Snowden-2.jpg',
+            'https://upload.wikimedia.org/wikipedia/en/e/eb/Heinz_Doofenshmirtz.png',
         scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
     },
     {
-        name: 'Hesso Nader',
+        name: 'Kim Possible',
         photo:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Aatef_Jenyat.jpg/293px-Aatef_Jenyat.jpg',
+            'https://upload.wikimedia.org/wikipedia/en/a/a3/Kim_possible_09.jpg',
         scores: [3, 3, 4, 1, 4, 3, 5, 3, 5, 4]
     },
     {
-        name: 'Sonneth The Swift',
+        name: 'Ron Stoppable',
         photo:
-            'http://www.billboard.com/files/styles/article_main_image/public/stylus/1235529-taylor-swift-woman-of-the-year-617-409.jpg',
+            'https://pbs.twimg.com/profile_images/378800000756113986/8e42d2ceba5d0abc63a0f44199736aab_400x400.jpeg',
         scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
     }
 ];
